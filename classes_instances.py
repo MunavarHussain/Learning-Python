0dc168db79798f6b28677a10a221494478f2cea8
MunavarHussain <armunavarhussain.be@gmail.com>
2019-07-24 12:20:44 +0530

    Create py file

---
+++ classes_instances.py
@@ -0,0 +1,5 @@
+#Tutorial 1 - Understanding classes and instances
+
+'''Object oriented programming is a way of programming, that in its unique way allows us to group data based on objects.It is indeed inspired from real world.
+Classes are blueprint associated with some data and functions. In OOP we call those attributes(variables) and methonds respectively.
+'''
