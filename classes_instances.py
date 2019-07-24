ee3c324f9b7aa9bb100e314e38e04086c5931874
Munavar Hussain <armunavarhussain.be@gmail.com>
2019-07-24 13:15:23 +0530

    Created two employee

--- classes_instances.py
+++ classes_instances.py
@@ -9,5 +9,20 @@ Let's create a employee class to understand the concept.
 
 '''
 class Employee(): # To define a class the keyword itself is 'class' with classname starting with Captial Letter. For other naming conventions: https://www.python.org/dev/peps/pep-0008/#naming-conventions
-  def __init__(self): #Its a constructor method, invoked whenever an object is created for this class. The keyword 'self' an instance of the class.
-    pass
+    def __init__(self,fname,lname,pay): #Its a constructor method, invoked whenever an object is created for this class. The keyword 'self' an instance of the class.
+        self.fname = fname 
+#       object.<somename> = parameter
+        self.lname = lname
+        self.pay = pay
+    def fullname(self):
+        return f"{self.fname} {self.lname}"
+
+if __name__ == "__main__":
+    emp1 = Employee('munavar','hussain',50000) # Creates an object of class employee
+    emp2 = Employee('ashraf','kutbudee',60000)
+    
+    print(emp1.fname)
+    print(emp2.fname)
+    print(emp1.fullname())
+#    Attributes and methods created so far are for instances not for the class. Hence,
+#    print(Employee.fname) throws error.
