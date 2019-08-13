4d20cf8d8fab73fda947f733d179a44e461c1655
Munavar Hussain <armunavarhussain.be@gmail.com>
2019-08-13 22:44:23 +0530

    added add & len dunder

--- magic_methods.py
+++ magic_methods.py
@@ -1,6 +1,6 @@
 #Tutorial 4 - Class inheritance
 
-''' Magic Methods are special methods, sometimes called dunder (with __<>__ format) has special functions
+''' Magic Methods are special methods, sometimes called dunder (with __dunder__ format) has special functions
 
 '''
 class Employee(): # To define a class the keyword itself is 'class' with classname starting with Captial Letter. For other naming conventions: https://www.python.org/dev/peps/pep-0008/#naming-conventions
@@ -14,12 +14,20 @@ class Employee(): # To define a class the keyword itself is 'class' with classna
         self.pay = pay
         self.email = fname + '.' + lname + '@company.com'
     
+    def fullname(self):
+        return self.fname+self.lname
     def __repr__(self): # for developers
         return f'Employee({self.fname},{self.lname},{self.pay})'
     
     def __str__(self): # for users
         return f'{self.fname} {self.lname} - {self.email}'
-        
+    
+    def __add__(self,other): #employee objects
+        return self.pay + other.pay
+    
+    def __len__(self):
+        return len(self.fullname())
+    
 if __name__ == "__main__":
     emp1 = Employee('munavar','hussain',50000) # Creates an object of class employee
     emp2 = Employee('ashraf','kutbudeen',60000)
@@ -33,3 +41,12 @@ if __name__ == "__main__":
     print(emp2.__str__())  
 #Employee(munavar,hussain,50000)
 #ashraf kutbudeen - ashraf.kutbudeen@company.com
+    print(emp1 + emp2)  #__add__ dunder similarly there are sub, mul and more @ https://docs.python.org/3/reference/datamodel.html#emulating-numeric-types
+# that's how integer addition is different from string addition in python. i.e int object and str object have different dunder __add__()
+    
+    print(len('mh')) #2 the keyword len is also a dunder method. it can be used as
+    print('mh'.__len__())
+    
+#    print(len(emp1)) object of type 'Employee' has no len() 
+#                       this shows up when len for employee is not declared
+    print(emp1.__len__()) #14
